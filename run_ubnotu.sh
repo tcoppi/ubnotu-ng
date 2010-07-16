4b5a82e42be09e3790ada4b9a0e1c41afb684e5a
Thomas Coppi <tcoppi@gmail.com>
2010-07-15 22:48:58 -0700

    Some cleanups

--- run_ubnotu.sh
+++ run_ubnotu.sh
@@ -7,13 +7,13 @@ PYTHONPATH="$PWD:$PYTHONPATH"
 
 SERVER="irc.freenode.net"
 PORT="7000"
-SSL="on"
+SSL="--ssl"
 NICK="ubnotung"
 IDENT="ubnotu"
 REALNAME="ubnotu-ng"
 NICKSERVPASS="urmom"
 CHANNELS="#ubnotu-ng"
 
-$PWD/ubnotu.py --server $SERVER --port $PORT --ssl $SSL --nickname $NICK \
+$PWD/ubnotu.py --server $SERVER --port $PORT $SSL --nickname $NICK \
 --ident $IDENT --realname $REALNAME --nickservpass = $NICKSERVPASS \
 --channels $CHANNELS
--- ubnotu.py
+++ ubnotu.py
@@ -10,7 +10,8 @@ opt.add_option("-s", "--server", dest="server", default="localhost",
         help="Server to connect to")
 opt.add_option("-p", "--port", dest="port", default="6667",
         help="Port to connect to on the server")
-opt.add_option("", "--ssl", dest="ssl", default="off", help="Use SSL or not")
+opt.add_option("", "--ssl", dest="ssl", action="store_true", default=False,
+        help="Use SSL or not")
 opt.add_option("-n", "--nickname", dest="nick", default="testbot", help="Bot \
 nickname")
 opt.add_option("-i", "--ident", dest="ident", default="test", help="Bot ident")
@@ -43,15 +44,15 @@ class ubnotu_ng:
             self.c = self.irc.server().connect(server = self.options.server,
                     port = int(self.options.port), nickname = self.options.nick,
                     username = self.options.ident, ircname =
-                    self.options.rname, ssl=True)
+                    self.options.rname, ssl=self.options.ssl)
         except irclib.ServerConnectionError, x:
             self.pprint(x)
             sys.exit(-1)
 
         self.pprint("Connection Successful!")
 
-        #FIXME XXX Just for testing
-        self.c.join("#ubnotu-ng")
+        for c in self.options.channels.split(","):
+            self.c.join(c)
 
         #point of no return
         self.pprint("Going into the rabbit hole", level=1)
